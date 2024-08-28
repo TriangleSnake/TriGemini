938fd84a01129c9f235fcbe5351c6ce452ad8567
TriangleSnake <trianglesnake2002@gmail.com>
2024-08-28 23:57:29 +0800

    Make words bigger

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-// 自動執行的匿名async函數
 (async () => {
     const session = await window.ai.createTextSession();
     var result = "";
@@ -6,14 +5,12 @@
     document.getElementById('queryInput').addEventListener('input', async function () {
         const query = this.value;
 
-        // 檢查是否有輸入，如果輸入框是空的，不發送請求
         if (!query) {
             document.getElementById('responseOutput').value = '';
             return;
         }
 
         try {
-            // Gemini API 呼叫的示例
             result = await session.prompt(query);
             document.getElementById('responseOutput').value = result;
         } catch (error) {
